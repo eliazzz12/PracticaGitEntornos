374e2154fe9cc8a448fb52af92cd4eebdc9cbba8
Elias Martín González <70772713+eliazzz12@users.noreply.github.com>
2024-02-24 10:34:36 +0100

    Linea 2 de codigo

--- ClaseB.java
+++ ClaseB.java
@@ -3,5 +3,6 @@ package practica;
 public class ClaseB {
 	public static void main(String[] args) {
 		System.out.println("Linea1");
+		System.out.println("Linea2");
 	}
 }
--- ClaseC.java
+++ ClaseC.java
@@ -3,5 +3,6 @@ package practica;
 public class ClaseC {
 	public static void main(String[] args) {
 		System.out.println("Linea1");
+		System.out.println("Linea2");
 	}
 }
